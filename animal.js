e74508f68c068707702480a0a4565f84bccde6bd
Yoana <116558529+yoanadivanova@users.noreply.github.com>
2023-01-18 20:38:05 +0100

    Add files via upload

---
+++ animal.js
@@ -0,0 +1,42 @@
+background(255, 255, 255);
+
+fill(254, 97, 187);
+noStroke();
+ellipse(200, 200, 200);
+
+stroke(254, 97, 187);
+beginShape();
+vertex(110, 160);
+vertex(135, 85);
+vertex(180, 130);
+endShape();
+
+beginShape();
+vertex(290, 160);
+vertex(265, 85);
+vertex(210, 130);
+endShape();
+
+fill(255, 255, 255);
+ellipse(165, 180, 25);
+ellipse(235, 180, 25);
+
+fill(0, 0, 0);
+noStroke();
+ellipse(165, 182, 15);
+ellipse(235, 182, 15);
+
+fill(254, 17, 187);
+ellipse(200, 220, 50, 35);
+
+fill(254, 97, 187);
+ellipse(190, 220, 10);
+ellipse(210, 220, 10);
+
+fill(60, 55, 60);
+ellipse(200, 260, 20, 25);
+
+fill(50, 45, 50);
+ellipse(200, 260, 18, 23);
+
+fill(40, 35, 40);
