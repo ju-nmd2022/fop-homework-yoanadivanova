d5481ddd76ba4f1d0393606bfd6df6e93eeeb949
Yoana <116558529+yoanadivanova@users.noreply.github.com>
2023-01-18 18:52:10 +0100

    Add files via upload

---
+++ emoji.js.js
@@ -0,0 +1,94 @@
+background(222, 222, 255);
+
+fill(255, 203, 76);
+noStroke();
+circle(200, 200, 180);
+
+fill(195, 59, 30);
+triangle(185, 185, 160, 165, 160, 200);
+triangle(215, 185, 240, 165, 240, 200);
+
+fill(255, 203, 76);
+triangle(175, 185, 160, 165, 160, 200);
+triangle(225, 185, 240, 165, 240, 200);
+
+fill(195, 59, 30);
+ellipse(210, 230, 16, 13);
+ellipse(211, 242, 16, 13);
+
+fill(255, 203, 76);
+ellipse(206, 230, 16, 13);
+ellipse(207, 242, 16, 13);
+
+fill(254, 97, 187);
+ellipse(250, 110, 20);
+ellipse(230, 110, 20);
+triangle(220, 113, 260, 113, 240, 140);
+ellipse(240, 110, 6);
+
+ellipse(250, 230, 12);
+ellipse(238, 230, 12);
+triangle(232, 232, 257, 230, 245, 245);
+
+ellipse(160, 85, 15);
+ellipse(145, 85, 15);
+triangle(137.5, 88, 167.5, 88, 153, 105);
+ellipse(153, 87, 6);
+
+ellipse(110, 200, 17);
+ellipse(93, 200, 17);
+triangle(118.5, 203, 84.5, 203, 101.5, 225);
+ellipse(101.5, 200, 6);
+
+/*ellipse(100, 110, 23);
+ellipse(80, 110, 23);
+triangle(68, 113, 112, 113, 91, 142);
+ellipse(101.5, 200, 6);
+
+fill(255, 255, 0);
+stroke(255, 255, 255);
+rect(110, 120, 30, 30);
+
+fill(255, 255, 255);
+ellipse(95, 110, 60);
+ellipse(155, 110, 60);
+ellipse(95, 160, 60);
+ellipse(155, 160, 60);*/
+
+fill(252, 248, 90);
+stroke(255, 255, 255);
+beginShape();
+vertex(115, 140);
+vertex(120, 110);
+vertex(125, 140);
+vertex(140, 142.5);
+vertex(125, 147.5);
+vertex(120, 175);
+vertex(115, 147.5);
+vertex(100, 142.5);
+vertex(115, 140);
+endShape();
+
+beginShape();
+vertex(250, 115);
+vertex(253, 105);
+vertex(256, 115);
+vertex(261, 118);
+vertex(256, 121);
+vertex(253, 131);
+vertex(250, 121);
+vertex(245, 118);
+vertex(250, 115);
+endShape();
+
+beginShape();
+vertex(305, 180);
+vertex(308, 165);
+vertex(311, 180);
+vertex(318, 183);
+vertex(311, 186);
+vertex(308, 201);
+vertex(305, 186);
+vertex(295, 183);
+vertex(305, 180);
+endShape();
